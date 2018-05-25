ac8eb2808a8134223102067e7d77fe2c4e2d369b
Ron DeMeritt <>
2018-05-25 15:38:07 -0400

    First commit

---
+++ config.py
@@ -0,0 +1,48 @@
+import argparse
+import re
+from rd_utilities import unix_time_now
+from log import build_logger, logging
+
+
+def build_arg_parser():
+    parser = argparse.ArgumentParser(
+        prog='ib_tool', description='CLI tool to place and manage orders in IB')
+    parser.add_argument('--log_level', help='Set the logging level')
+    parser.add_argument('--key', help='IB API key', required=True)
+
+    return parser.parse_args()
+
+
+def init():
+    global session
+    global start_time
+    global logger
+    global args
+
+    start_time = unix_time_now()
+    args = build_arg_parser()
+
+    if args.log_level:
+        global log_level
+        log_level = getattr(logging, args.log_level.upper())
+    logger = build_logger()
+
+    # process our arguments
+    if args.key:
+        pass
+    # drop out if we don't have a way to setup session
+    else:
+        logger.error('No key provided')
+        exit(1)
+
+    # check to see if our session was successfully created
+    if not session:
+        exit(2)
+
+
+# define global variables
+start_time = None
+logger = None
+log_level = logging.INFO
+args = None
+session = None
---
+++ rd_utilities.py
@@ -0,0 +1,10 @@
+"""
+common functions and utilities that i use...
+"""
+
+from time import time
+import json
+
+
+def unix_time_now():
+    return int(time())
